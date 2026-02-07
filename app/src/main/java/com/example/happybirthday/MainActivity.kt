f4ca0310ba7e3aaf48d491b971ada029ff739295
2839-23 <klimkina53@gmail.com>
2026-02-07 10:20:10 +0300

    Update greeting text

--- app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -12,8 +15,11 @@ import androidx.compose.ui.unit.sp
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.happybirthday.ui.theme.HappyBirthdayTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,7 +29,13 @@ class MainActivity : ComponentActivity() {
         setContent {
             HappyBirthdayTheme {
                 Surface(modifier = Modifier.fillMaxSize(),
-                color = MaterialTheme.colorScheme.background){}
+                color = MaterialTheme.colorScheme.background){
+                    GreetingText(
+                        message = "Happy Birthday Sam!",
+                        from = "From Emma",
+                        modifier = Modifier.padding(8.dp)
+                    )
+                }
 //                { innerPadding ->
 //                    Greeting(
 //                        name = "Android",
@@ -44,18 +56,35 @@ class MainActivity : ComponentActivity() {
 //}
 
 @Composable
-fun GreetingText(message: String, modifier: Modifier = Modifier)
+fun GreetingText(message: String, from: String, modifier: Modifier = Modifier)
 {
-    Text(text = message,
-    fontSize = 100.sp,
-    lineHeight = 116.sp)
+    Column( verticalArrangement = Arrangement.Center,
+        modifier = modifier)
+    {
+
+        Text(
+            text = message,
+            fontSize = 100.sp,
+            lineHeight = 116.sp,
+            textAlign = TextAlign.Center
+        )
+        Text(
+            text = from,
+            fontSize = 20.sp,
+            modifier = Modifier
+                .padding(16.dp)
+                .align(alignment = Alignment.End)
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun BirthdayCardPreview() {
     HappyBirthdayTheme {
-        GreetingText(message = "Happy Birthday Sam!")
+        GreetingText(message = "Happy Birthday Sam!",
+        from = "From Emma",
+            )
 //        Greeting("Yliana_Olia")
     }
 }

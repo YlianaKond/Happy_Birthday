9916352d2db6f4b714eeb72000b9abbe6b67f9c1
2839-23 <klimkina53@gmail.com>
2026-02-07 10:58:04 +0300

    Update greeting text1

--- app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -3,13 +3,13 @@ package com.example.happybirthday
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.ui.unit.sp
 import androidx.compose.material3.Surface
@@ -65,7 +65,7 @@ fun GreetingText(message: String, from: String, modifier: Modifier = Modifier)
         Text(
             text = message,
             fontSize = 100.sp,
-            lineHeight = 116.sp,
+            lineHeight = 117.sp,
             textAlign = TextAlign.Center
         )
         Text(

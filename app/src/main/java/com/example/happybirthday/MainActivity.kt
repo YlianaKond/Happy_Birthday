5ee949145d0387d0718d059b6327b87e54eed6e6
2839-23 <klimkina53@gmail.com>
2026-02-14 09:42:50 +0300

    Add background image

--- app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.happybirthday
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -17,6 +18,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.material3.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -77,12 +79,21 @@ fun GreetingText(message: String, from: String, modifier: Modifier = Modifier)
         )
     }
 }
+@Composable
+fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier)
+{
+    val image = painterResource(R.drawable.androidparty)
+    Image (
+        painter = image,
+        contentDescription = null
+    )
+}
 
 @Preview(showBackground = true)
 @Composable
 fun BirthdayCardPreview() {
     HappyBirthdayTheme {
-        GreetingText(message = "Happy Birthday Sam!",
+        GreetingImage(message = "Happy Birthday Sam!",
         from = "From Emma",
             )
 //        Greeting("Yliana_Olia")

831cb7c72079e853e6d9817656b9163bedb692db
2839-23 <klimkina53@gmail.com>
2026-02-14 10:04:00 +0300

    Add resources strings

--- app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -21,6 +21,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -77,7 +78,7 @@ fun GreetingText(message: String, from: String, modifier: Modifier = Modifier)
             fontSize = 20.sp,
             modifier = Modifier
                 .padding(16.dp)
-                .align(alignment = Alignment.End)
+                .align(alignment = Alignment.End) // спросить
         )
     }
 }
@@ -106,9 +107,10 @@ fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier)
 @Composable
 fun BirthdayCardPreview() {
     HappyBirthdayTheme {
-        GreetingImage(message = "Happy Birthday Sam!",
-        from = "From Emma",
-            )
+        GreetingImage(
+            message = stringResource(R.string.happy_birthday_text),
+            from = stringResource(R.string.signature_text),
+        )
 //        Greeting("Yliana_Olia")
     }
 }
